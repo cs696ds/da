e65eaf776f050d07945e45a5d9228b84b4c380c2
Hee Hwang (Peter) <heehcs@gmail.com>
2021-02-18 11:16:11 -0500

    Add passage-level tokenization

---
+++ src/python/psg_tokenizer.py
@@ -0,0 +1,57 @@
+import pdb
+import json
+import time
+import math
+import sys
+import os
+from allennlp.data.tokenizers.word_tokenizer import WordTokenizer
+
+
+wt = WordTokenizer()
+
+LIMIT = 1
+for i in range(int(sys.argv[1]), int(sys.argv[1]) + LIMIT):
+    with open('part-%05d' % i, 'r') as f:
+        data = f.read().split('\n')
+
+    data = [x for x in data if len(x.strip()) > 0]
+    start = time.time()
+    docs = [] 
+    val_url  = ""
+    val_s3   = ""
+    val_text = ""
+    for j, d in enumerate(data):
+        if j == 10:
+            break
+        print('%d / %d' % (j, len(data)))
+        if d.startswith("# newdoc"):
+            pos = d.find('s3 =')
+            val_url = d[14:pos].strip()
+            val_s3  = d[pos + 4:].strip()
+            if len(val_text.strip()) > 0:
+                dict = {"url" : val_url,
+                        "s3"  : val_s3,
+                        "text": val_text
+                }
+                docs.append(json.dumps(dict))
+                val_url = ""
+                val_text = ""
+        else:
+            tokenized_str = wt.tokenize(d)
+            line = ' '.join(w.text for w in tokenized_str)
+            line += ' '
+            val_text += line
+
+    with open('depcc.%05d-of-19101.jsonl' % i, 'w') as f:
+        for doc in docs:
+            f.write(doc);
+            f.write('\n')
+            
+    print("%.4f seconds for %d" % (time.time() - start, i))
+    sys.exit()
+#    final_data = [' '.join([y.text for y in x]) for x in tokenized_strs]
+
+    # with open('depcc.%05d-of-19101' % i, 'w') as f:
+    #     f.write('\n'.join(final_data))
+
+#    os.remove('part-%05d' % i)
