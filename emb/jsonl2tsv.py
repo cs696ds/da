1f8a52b40575e0a801c98877acaeefb899e51cfe
Hee Hwang (Peter) <heehcs@gmail.com>
2021-02-19 12:21:46 -0500

    add jsonl2tsv utility file

---
+++ emb/jsonl2tsv.py
@@ -0,0 +1,20 @@
+import pdb
+import sys
+import json
+import csv
+LIMIT = 1
+
+for i in range(int(sys.argv[1]), int(sys.argv[1]) + LIMIT):
+    json_lines = []
+    with open('part-%05d.jsonl' % i, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            j = json.loads(line)
+            json_lines.append(j)
+    with open('part-%05d.tsv'   % i, 'w') as output_file:
+        dw = csv.DictWriter(output_file, json_lines[0].keys(), delimiter='\t')
+        dw.writeheader()
+        for j in json_lines:
+            dw.writerow(j)
+
+
