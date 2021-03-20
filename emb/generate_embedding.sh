150a578da3fb2153aef35a279ea4f6ce9ab2f5c2
Hee Hwang (Peter) <heehcs@gmail.com>
2021-03-20 08:37:21 -0400

    change project directory

--- emb/generate_embedding.sh
+++ emb/generate_embedding.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 /home/heeh/Projects/da/emb
 
-PROJ_HOME="/home/heeh/Projects"
+#PROJ_HOME="/home/heeh/Projects"
+PROJ_HOME="/mnt/nfs/work1/696ds-s21/hhwang"
 
 python ${PROJ_HOME}/DPR/generate_dense_embeddings.py \
        --out_file "${PROJ_HOME}/da/emb/${1}" \
