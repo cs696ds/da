1a11ea915524fb0be82803e0a4c99e5c5596b09c
Hee Hwang (Peter) <heehcs@gmail.com>
2021-03-20 15:09:58 -0400

    change post target

--- src/sh/4_solr_post_data.sh
+++ src/sh/4_solr_post_data.sh
@@ -2,4 +2,4 @@
 # Index data files
 # bin/post -c <core name> <file name>
 #../../../solr-8.8.0/bin/post -c depcc ../../data/98-depcc-20/*
-../../../solr-8.8.0/bin/post -c depcc ../../data/99-depcc/* 
+../../../solr-8.8.0/bin/post -c depcc-small ../../data/99-depcc/01000-01999/
