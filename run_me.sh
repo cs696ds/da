19d227f916879ed8826dee21098638bf3135e5b5
Hee Hwang (Peter) <heehcs@gmail.com>
2021-03-24 15:32:21 -0400

    Add run_me.sh

---
+++ run_me.sh
@@ -0,0 +1,17 @@
+python dense.py --dataset_name 02_acl --train_file data/02-acl-arc/train.jsonl > 02_acl.txt
+
+
+python dense.py --dataset_name 02_acl --train_file data/02-acl-arc/train.jsonl > 02_acl.txt
+
+python dense.py --dataset_name 02_acl --train_file data/02-acl-arc/train.jsonl > 02_acl.txt
+
+
+python sparse.py --dataset_name 02_acl --train_file data/02-acl-arc/train.jsonl > 02_acl.txt
+
+python sparse.py --dataset_name 02_acl --train_file data/02-acl-arc/train.jsonl > 02_acl.txt
+
+python sparse.py --dataset_name 02_acl --train_file data/02-acl-arc/train.jsonl > 02_acl.txt
+
+
+
+
