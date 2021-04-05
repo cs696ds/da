92d7f58917e1905543fa286087c61a6474737795
Sudarshan Raghavan <sudarshan2009@hotmail.com>
2021-04-05 12:15:08 -0400

    deleted incorrect data

--- create_new_train.py
+++ create_new_train.py
@@ -12,9 +12,14 @@ for task in tasks:
 	print("Processing task: ",task)
 	train_data = open(os.path.join(train, task, "train.jsonl")).readlines()
 	same_label_data = open(os.path.join(same_label, task, same_label + "_" + task + ".jsonl")).readlines()
-	train_data.extend(same_label_data)
-	print("Length of new train set is: ",len(train_data))
+	
+	temp_new_train = []
+	for data_coll in [train_data, same_label_data]:
+		for item in data_coll:
+			temp_new_train.append(json.dumps(eval(item)))
+
+	print("Length of new train set is: ",len(temp_new_train))
 
 	with open(os.path.join(new_train_same_label, task, "same_label_newtrain.jsonl"), 'w') as filewriter:
-		for item in train_data:
+		for item in temp_new_train:
 			filewriter.write('%s\n' % item)
