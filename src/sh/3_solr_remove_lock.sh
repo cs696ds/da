509c9c12c811462c8263d586b911269bb1d450d8
Hee Hwang (Peter) <heehcs@gmail.com>
2021-04-18 10:56:36 -0400

    update

--- src/sh/3_solr_remove_lock.sh
+++ src/sh/3_solr_remove_lock.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-rm ../../../solr-8.8.0/server/solr/depcc/data/index/write.lock 
+rm ../../../solr-8.8.0/server/solr/depcc/data/index/write.lock
+rm ../../../solr-8.8.0/server/solr/depcc/data-small/index/write.lock
+rm ../../../solr-8.8.0/server/solr/depcc/data-large/index/write.lock 
