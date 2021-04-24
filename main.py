1d0002b3bd5eb36c17783f2d7213712e7826a6f2
Hee Hwang (Peter) <heehcs@gmail.com>
2021-04-24 10:29:40 -0400

    clean up

--- main.py
+++ main.py
@@ -51,8 +51,6 @@ def main(args):
         query_docs[i] = sent_tokenize(json_lines[i]['text'])
         qlen_vec[i]   = len(query_docs[i])
     AVG_TOK_LEN = np.rint(np.mean(qlen_vec))
-    # query_docs[182] = sent_tokenize(json_lines[182]['text'])
-    # AVG_TOK_LEN = 44
     print('Average Token Length: %d' % AVG_TOK_LEN )
     ################################################################################
     # Segment Raw Queries
@@ -86,10 +84,10 @@ def main(args):
         ################################################################################
         # Retrieve CC documents
         ################################################################################
-        #solr_select = 'http://localhost:8983/solr/depcc-large/select?q='
+        # solr_select = 'http://localhost:8983/solr/depcc-large/select?q='
         solr_select = 'http://localhost:8983/solr/depcc-small/select?q='
         # solr_select = 'http://localhost:8983/solr/depcc-large/select?q='
-        #    solr_select = 'http://localhost:8983/solr/depcc-small/select?fl=score%2C*&q='
+        # solr_select = 'http://localhost:8983/solr/depcc-small/select?fl=score%2C*&q='
         cc_psgs = []
         print('Process NUM_SEG_QUERIES: %d' % NUM_SEG_QUERIES)
         for j in range(NUM_SEG_QUERIES):
@@ -104,30 +102,24 @@ def main(args):
             for k in range(args.max_doc):
                 try:
                     cur = json.loads(retrieved_docs[k]['_src_'])['text']
-                    #print(retrieved_docs[k]['_src_'])
                     if len(cc_docs_raw) + len(cur) > 1000000:
                         break
                     else:
                         cc_docs_raw += cur
                 except ValueError:
                     continue
-            print(cc_docs_raw)
             if len(cc_docs_raw) == 0:
                 continue
             ################################################################################
             # Segmented Retrieved Documents into CC passages
             ################################################################################
             cc_docs_tok = sent_tokenize(cc_docs_raw)
-            # print(cc_docs_raw)
-            # print(cc_docs_tok)
-            # print(len(cc_docs_tok))
             if len(cc_docs_tok) < 100:
                 cc_psgs.append({'doc_id' : '', 'doc_text'  : cc_docs_raw,  'title': ''  })
             else:
                 temp = ''
                 num_tokens = 0
                 for sent in cc_docs_tok.sents:
-                    #print(sent.text)
                     if num_tokens < AVG_TOK_LEN:
                         temp += sent.text
                         num_tokens += len(sent)
@@ -139,15 +131,8 @@ def main(args):
                     cc_psgs.append({'doc_id' : '', 'doc_text'  : temp,  'title': ''  })
                     temp = ''
                     num_tokens = 0
-        # Handle exception
-        if len(cc_psgs) == 0:
+        if len(cc_psgs) == 0:  # Handle exception
             continue
-        # END of for j in tqdm(range(NUM_SEG_QUERIES)):
-        #print('Length of cc_psgs')
-        #print(len(cc_psgs))
-        # for c in cc_psgs:
-        #     print(c)
-        #     print()
         ################################################################################
         # Prepare Train and CC passages for sorting
         ################################################################################
@@ -156,17 +141,10 @@ def main(args):
         query_psgs = []
         query_dict = {'doc_id': str(i), 'doc_text': json_lines[i]['text'], 'title': ''}
         query_psgs.append(query_dict)
-
-        # print(query_psgs[0]['doc_text'][:50])
-        # print(cc_psgs[0]['doc_text'][:50])
-
         TR = 'tr_' + args.dataset_name
         CC = 'cc_' + args.dataset_name
         TR_TSV  = 'emb/' + TR + '.tsv' 
         CC_TSV  = 'emb/' + CC + '.tsv'
-        #print(TR)
-        #print(textwrap.fill(query_psgs[0]['doc_text'], 80))
-        #print()
         with open(TR_TSV, 'w') as q_csv_file:
             q_dw = csv.DictWriter(q_csv_file, query_psgs[0].keys(), delimiter='\t')
             for q_psg in query_psgs:
@@ -183,28 +161,24 @@ def main(args):
         print(MAX_CC_PSGS)
         nq = len(query_psgs)  # query size
         nb = len(cc_psgs) # database size
+        # Dense Passage Representation(DPR)
         if args.emb=='dense':
-            # Dense Passage Representation(DPR)
             subprocess.call(['emb/generate_embedding.sh', TR])
             subprocess.call(['emb/generate_embedding.sh', CC])
             # FAISS Preparation
             query_embeddings = np.load('emb/' + TR + '_0.pkl', allow_pickle=True)
             cc_embeddings = np.load('emb/' + CC + '_0.pkl', allow_pickle=True)
-
             if nq != len(query_embeddings):
                 print('Query Embedding generation failed.')
                 continue
             if nb != len(cc_embeddings):
                 print('CC Embedding generation failed.')
                 continue
-            
             d = query_embeddings[0][1].size
             xq = np.zeros((nq,d), dtype='float32')
             for l in range(nq):
                 xq[l] = query_embeddings[l][1]
             xb = np.zeros((nb,d), dtype='float32')
-            # print(nb)
-            # print(len(cc_embeddings))
             for l in range(nb):
                 xb[l] = cc_embeddings[l][1]
         # if args.emb=='sparse':
@@ -222,95 +196,35 @@ def main(args):
         #     print(cc_embeddings.shape)
         #     xq = np.array(query_embeddings, dtype='float32')
         #     xb = np.array(cc_embeddings, dtype='float32')
-        # print('Number of query passages: %d' % nq)
-        # print(xq)
         index = faiss.IndexFlatL2(d)   # build the index
-        # print('trained? %r' % index.is_trained)
         index.add(xb)                  # add vectors to the index
-
-        # print('Total number of indexed CC passages: ', index.ntotal)
-        # print()
-        # print('Using an indentical CC set')
-        # k = nb                          # we want to see 4 nearest neighbors
-        # D, I = index.search(xb[:5], k) # sanity check
-        # print('================================================================')
-        # print('4 nearest neighbors(sanity check)')
-        # print(I)
-        # print()
-        # print('distances(sanity check)')
-        # print(D)
-        # print()
-        
-        # print('===============================================================')
-        # print('Using the query')
         k = nb
         D, I = index.search(xq, k)     # actual search
-        # print('Nearest neighbors for query')
-        # print(I)                 
-        # print('\ndistances')
-        # print(D)
-        # print()
-        # print('Train passage')
-        # print(textwrap.fill(query_psgs[0]['doc_text'],80))
-        # print()
-        
-        # print('CLOSEST passages in CC:')
-        # for l in range(4):
-        #     print('-------------------------------------------------------------')
-        #     print('Closest %d' % i)
-        #     closest = I[0][l]
-        #     print(textwrap.fill(cc_psgs[closest]['doc_text'], 80))
-        #     print('------------------------------------------------------------')
-        # print('...')
-        # for l in range(MAX_CC_PSGS-4, MAX_CC_PSGS):
-        #     print('-------------------------------------------------------------')
-        #     print('Farthest %d' % i)
-        #     closest = I[0][l]
-        #     print(textwrap.fill(cc_psgs[closest]['doc_text'],80))
-
-
         ################################################################################
         # Write Augmented Passages
         ################################################################################
         knn_indices = I[0].astype(int)
-        #        print(knn_indices)
-#        cc_psgs_jsonl = []
+        LEN_CC_PSGS = len(knn_indices)
         sorted_cc_psgs_jsonl = []
-        # print(I[0])
-        # print(D[0])
-        # sys.exit()
-        # for psg in cc_psgs:
-        #     d = {"text" : psg['doc_text'],
-        #          "label"  : "",
-        #          "metadata": {}
-        #          }
-        #     cc_psgs_jsonl.append(json.dumps(d))
 
-        for k in knn_indices:
-            d = {"text" : cc_psgs[k]['doc_text'],
+        for j in range(LEN_CC_PSGS):
+            d = {"text" : cc_psgs[knn_indices[j]]['doc_text'],
                  "label"  : "",
                  "metadata": {},
-                 "score": str(D[0][k])
+                 "score": str(D[0][j])
                  }
             sorted_cc_psgs_jsonl.append(json.dumps(d))
-#        print(sorted_cc_psgs_jsonl)
-#        sys.exit()
         with open('aug_sorted/' + args.dataset_name + '/%05d.jsonl' % i, 'w') as f:
-            for i in range(len(sorted_cc_psgs_jsonl)):
-                f.write(sorted_cc_psgs_jsonl[i]);
+            for j in range(len(sorted_cc_psgs_jsonl)):
+                f.write(sorted_cc_psgs_jsonl[j]);
                 f.write('\n')
-
-
-            # for k in knn_indices:
-            #     f.write(cc_psgs_jsonl[k]);
-            #     f.write('\n')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='DA')
     parser.add_argument("--max_doc", default=1, type=int, help="")    
     parser.add_argument("--dataset_name", default="citation_intent", type=str, help="")
 #    parser.add_argument("--dataset_name", default="hyperpartisan_news"                     , type=str, help="")
-    #    parser.add_argument("--dataset_name", default="imdb"                     , type=str, help="")
+#    parser.add_argument("--dataset_name", default="imdb"                     , type=str, help="")
 #    parser.add_argument("--query_files"  , default="data/citation_intent/train.jsonl", type=str, help="")
 #parser.add_argument("--query_files"  , default="failed_tests_by_base_models/failed_citation_intent.jsonl", type=str, help="")
     # parser.add_argument("--query_files"  , default="failed_tests_by_base_models/failed_hyperpartisan_news.jsonl", type=str, help="")
